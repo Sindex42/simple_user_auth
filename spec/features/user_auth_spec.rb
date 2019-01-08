9637f5915b123f9d65199ec4533eec75ed13e2f0
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-08 14:56:48 +0000

    Add feature test - User can't sign up twice with the same email address

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,13 +1,16 @@
+require './spec/features/web_helper'
+
 RSpec.feature 'User Authentication' do
   context 'Sign Up' do
-    scenario 'A user can signup' do
-      visit '/'
-      click_on 'Sign up'
-      fill_in :email, with: 'test@test.com'
-      fill_in :password, with: 'secret123'
-      click_button 'Sign up'
+    before { signup }
 
+    scenario 'A user can signup' do
       expect(page).to have_content 'Welcome, test@test.com'
+    end
+
+    scenario "A user can't sign up twice with the same e-mail address" do
+      signup
+      expect(page).to have_content "user name already exists"
     end
   end
 
