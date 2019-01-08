ed2424c5de16709d5a3baa12d560b65835e084a0
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-08 16:07:19 +0000

    Add feature test testing password length

--- lib/user.rb
+++ lib/user.rb
@@ -4,7 +4,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true
   property :password, BCryptHash
 
   def self.authenticate(email, password)
@@ -19,6 +19,6 @@ class User
   end
 
   def self.exists?(email)
-    user = first(email: email) ? true : false
+    first(email: email) ? true : false
   end
 end
--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -2,15 +2,24 @@ require './spec/features/web_helper'
 
 RSpec.feature 'User Authentication' do
   context 'Sign Up' do
-    before { signup }
-
     scenario 'A user can signup' do
+      signup
       expect(page).to have_content 'Welcome, test@test.com'
     end
 
     scenario "A user can't sign up twice with the same e-mail address" do
       signup
-      expect(page).to have_content "Error, email already exists"
+      signup
+      expect(page).to have_content "Error, email or password invalid"
+    end
+
+    scenario "A user's password has to be at least 6 characters long" do
+      visit '/'
+      click_on 'Sign in'
+      fill_in :email, with: 'test@test.com'
+      fill_in :password, with: 'short'
+      click_button 'Sign in'
+      expect(page).to have_content "Error, email or password invalid"
     end
   end
 
